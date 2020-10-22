38daaa16ce21ab5706d3b175fc4baef65e54ff08
Pontus Östlund <ponost@roxen.com>
2020-10-23 00:24:53 +0200

    added compileString() as well

--- src/lib/compiler.ts
+++ src/lib/compiler.ts
@@ -1,15 +1,19 @@
 import { existsSync, readFileSync } from 'fs'
 import { CodeGenerator } from './codegen'
 import { Parser } from './parser'
-import { Tokenizer } from '../mod'
+import { Tokenizer } from './tokenizer'
 
 export function compileFile(path: string): string {
   if (!existsSync(path)) {
     console.error(`No such file %O`, path)
   }
 
+  return compileString(readFileSync(path))
+}
+
+export function compileString(bitsyCode: string | Buffer): string {
   const gen = new CodeGenerator()
-  const tok = new Tokenizer(readFileSync(path))
+  const tok = new Tokenizer(bitsyCode)
   const parser = new Parser(tok, gen)
   parser.parse()
 
